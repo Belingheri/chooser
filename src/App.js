3ad969f75037c9dcb597f1d31323c750fe696bf7
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-11-26 21:55:57 +0100

    creazione classe Attributo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import Attributo from "./model/Attributo";
 
-const defaultAttributi = [
-  { nome: "consumo", valore: 7 },
-  { nome: "potenza", valore: 3 },
-];
+const defaultAttributi = [];
+
+const a = new Attributo("consumo", 7);
+const b = new Attributo("potenza", 3);
+
+defaultAttributi.push(a);
+defaultAttributi.push(b);
+
+//   { nome: "consumo", valore: 7 },
+//   { nome: "potenza", valore: 3 },
+// ];
 
 const defaultElementi = [
   {
@@ -32,23 +40,38 @@ const defaultElementi = [
 function App() {
   const [attributi, setAttributi] = useState(defaultAttributi);
   const [elementi, setElementi] = useState(defaultElementi);
+  const [attributiErrors, setAttributiErrors] = useState({});
 
   const getValore = (oggetto, attributi) => {
     let dividendo = 0,
       divisore = 0;
     Object.keys(oggetto.attributi).forEach((nomeAttributo) => {
       const attributo = attributi.find((attr) => attr.nome === nomeAttributo);
-      dividendo += oggetto.attributi[nomeAttributo] * attributo.valore;
-      divisore += attributo.valore;
+      dividendo += oggetto.attributi[nomeAttributo] * attributo.peso;
+      divisore += attributo.peso;
     });
     return dividendo / divisore;
   };
 
   const handleChangeAttributoValue = (attributo, valore) => {
-    const attualiAttributi = [...attributi];
-    const idx = attualiAttributi.findIndex((attr) => attr === attributo);
-    attualiAttributi[idx].valore = valore;
-    setAttributi(attualiAttributi);
+    try {
+      const attualiAttributi = [...attributi];
+      const idx = attualiAttributi.findIndex(
+        (attr) => attr.nome === attributo.nome
+      );
+      attualiAttributi[idx].peso = valore;
+      setAttributi(attualiAttributi);
+      resetAttributoError(attributo.nome);
+    } catch (error) {
+      const actualErrors = { ...attributiErrors };
+      actualErrors[attributo.nome] = error.message;
+      setAttributiErrors(actualErrors);
+    }
+  };
+  const resetAttributoError = (nomeAttributo) => {
+    const actualErrors = { ...attributiErrors };
+    delete actualErrors[nomeAttributo];
+    setAttributiErrors(actualErrors);
   };
 
   const handleChangeElementoValue = (elemento, nomeAttributo, valore) => {
@@ -71,13 +94,16 @@ function App() {
               <input
                 type="number"
                 min="1"
-                value={attributo.valore}
+                value={attributo.peso}
                 onChange={({ currentTarget: t }) => {
-                  if (!t.valueAsNumber || t.valueAsNumber < 1) return;
                   handleChangeAttributoValue(attributo, t.valueAsNumber);
                 }}
+                onBlur={() => resetAttributoError(attributo.nome)}
               />
             </span>
+            {attributiErrors[attributo.nome] && (
+              <span>{attributiErrors[attributo.nome]}</span>
+            )}
           </li>
         ))}
       </ol>
---
+++ src/model/Attributo.js
@@ -0,0 +1,40 @@
+const _nome = new WeakMap();
+const _peso = new WeakMap();
+
+/**
+ * Attributo
+ * @description attributo, caratteriristica di un elemento
+ * @author Belingheri
+ *
+ */
+class Attributo {
+  /**
+   *
+   * @param {string} nome nome dell'attributo
+   * @param {number} peso peso dell'attributo compreso tra 1 e 10
+   */
+  constructor(nome, peso) {
+    this.nome = nome;
+    this.peso = peso;
+  }
+
+  set nome(nome) {
+    if (typeof nome !== "string")
+      throw new Error("nome deve essere una stringa");
+    _nome.set(this, nome);
+  }
+  get nome() {
+    return _nome.get(this);
+  }
+  set peso(peso) {
+    if (typeof peso !== "number") throw new Error("peso deve essere un numero");
+    if (peso < 0 || peso > 10)
+      throw new Error("peso deve essere compreso tra 1 e 10");
+    _peso.set(this, peso);
+  }
+  get peso() {
+    return _peso.get(this);
+  }
+}
+
+export default Attributo;
