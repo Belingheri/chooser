f7f91e155a729536dd4c444f9a14410316c74ddb
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-11-29 15:05:13 +0100

    use AttributoR and AttributoFrom

--- src/components/Attributi.jsx
+++ src/components/Attributi.jsx
@@ -1,91 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { ListGroup } from "react-bootstrap";
 
 import Attributo from "./../model/Attributo";
+import AttributoR from "./AttributoR";
+import AttributoForm from "./AttributoForm";
 
-function Attributi({
-  attributi,
-  onAdd,
-  onRemove,
-  onChange,
-  attributiErrors,
-  onFocusOut,
-}) {
-  const [attributoForm, setAttributoForm] = useState({
-    nome: "",
-    peso: Attributo.pesoMinimo,
-  });
-
-  const changeFormAttributo = ({ currentTarget: t }) => {
-    const attributo = { ...attributoForm };
-    attributo[t.name] = t.name === "nome" ? t.value : t.valueAsNumber;
-    setAttributoForm(attributo);
-  };
-
-  const handleCreaAttributo = (e) => {
-    e.preventDefault();
-    const attributo = { ...attributoForm };
-    try {
-      const attr = new Attributo(attributoForm.nome, attributoForm.peso);
-      const giaPresente = attributi.find((e) => e.nome === attr.nome);
-      if (giaPresente) throw new Error(`Attributo ${attr.nome} gia presente`);
-      onAdd(attr);
-      setAttributoForm({ nome: "", peso: Attributo.pesoMinimo });
-    } catch (error) {
-      attributo.error = error.message;
-      setAttributoForm(attributo);
-    }
-  };
-
+function Attributi({ attributi, onAdd, onRemove, onChange }) {
   const attributiOrdinati = attributi.sort((a, b) => b.peso - a.peso);
   return (
     <div>
-      <ol>
+      <h1>Attributi</h1>
+
+      <ListGroup>
         {attributiOrdinati.map((attributo) => (
-          <li key={attributo.nome}>
-            {attributo.nome}{" "}
-            <span>
-              <input
-                type="number"
-                value={attributo.peso}
-                onChange={({ currentTarget: t }) => {
-                  onChange(attributo, t.valueAsNumber);
-                }}
-                onBlur={() => onFocusOut(attributo.nome)}
-              />
-            </span>
-            {attributi.length > 1 && (
-              <button onClick={() => onRemove(attributo.nome)}>rimuovi</button>
-            )}
-            {attributiErrors[attributo.nome] && (
-              <span>{attributiErrors[attributo.nome]}</span>
-            )}
-          </li>
+          <AttributoR
+            key={attributo.nome}
+            attributo={attributo}
+            onChange={onChange}
+            onRemove={onRemove}
+            canRemove={attributi.length > 1}
+          />
         ))}
-      </ol>
-      <form onSubmit={handleCreaAttributo}>
-        <label htmlFor="nome">Nome</label>
-        <input
-          type="text"
-          id="nome"
-          name="nome"
-          placeholder="Nome Attributo"
-          value={attributoForm.nome}
-          onChange={changeFormAttributo}
-        />
-        <br />
-        <label htmlFor="peso">Peso:</label>
-        <input
-          type="number"
-          id="peso"
-          name="peso"
-          value={attributoForm.peso}
-          onChange={changeFormAttributo}
-        />
-        <br />
-        <button type="submit">aggiungi</button>
-        {attributoForm.error && <span>{attributoForm.error}</span>}
-      </form>
+      </ListGroup>
+
+      <AttributoForm attributi={attributi} onAdd={onAdd} />
     </div>
   );
 }
@@ -95,8 +34,6 @@ Attributi.propTypes = {
   onAdd: PropTypes.func.isRequired,
   onRemove: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
-  onFocusOut: PropTypes.func.isRequired,
-  attributiErrors: PropTypes.object.isRequired,
 };
 
 export default Attributi;
