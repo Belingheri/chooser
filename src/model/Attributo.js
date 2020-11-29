9e16b86725d8d7567026e4d8ada9d8b246bb63cc
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-11-29 15:03:48 +0100

    create Separate validation function

--- src/model/Attributo.js
+++ src/model/Attributo.js
@@ -18,6 +18,35 @@ class Attributo {
   static pesoMassimo = 10;
 
   /**
+   * validatePeso
+   * @Belingheri
+   * @description valida il peso se invalido solleva un eccezione
+   * @param {number} peso peso da validare
+   * @throws causa di non validita'
+   */
+  static validatePeso(peso) {
+    if (!peso) throw new Error("peso deve essere valorizzato");
+    if (typeof peso !== "number") throw new Error("peso deve essere un numero");
+    if (peso < Attributo.pesoMinimo || peso > Attributo.pesoMassimo)
+      throw new Error(
+        `peso deve essere compreso tra ${Attributo.pesoMinimo} e ${Attributo.pesoMassimo}`
+      );
+  }
+
+  /**
+   * validateNome
+   * @Belingheri
+   * @description valida il peso se invalido solleva un eccezione
+   * @param {string} nome nome da validare
+   * @throws causa di non validita'
+   */
+  static validateNome(nome) {
+    if (!nome) throw new Error("nome deve essere valorizzato");
+    if (typeof nome !== "string")
+      throw new Error("nome deve essere una stringa");
+  }
+
+  /**
    *
    * @param {string} nome nome dell'attributo
    * @param {number} peso peso dell'attributo compreso tra Attributo.pesoMinimo e Attributo.pesoMassimo
@@ -28,21 +57,14 @@ class Attributo {
   }
 
   set nome(nome) {
-    if (!nome) throw new Error("nome deve essere valorizzato");
-    if (typeof nome !== "string")
-      throw new Error("nome deve essere una stringa");
+    Attributo.validateNome(nome);
     _nome.set(this, nome);
   }
   get nome() {
     return _nome.get(this);
   }
   set peso(peso) {
-    if (!peso) throw new Error("peso deve essere valorizzato");
-    if (typeof peso !== "number") throw new Error("peso deve essere un numero");
-    if (peso < Attributo.pesoMinimo || peso > Attributo.pesoMassimo)
-      throw new Error(
-        `peso deve essere compreso tra ${Attributo.pesoMinimo} e ${Attributo.pesoMassimo}`
-      );
+    Attributo.validatePeso(peso);
     _peso.set(this, peso);
   }
   get peso() {
