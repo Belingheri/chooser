4939cb0b7a17d3b61e89a9979c7406d94f6db9df
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-12-01 18:52:20 +0100

    fix add/remove attributi non visualizza in elemenr

--- src/components/ElementoR.jsx
+++ src/components/ElementoR.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { InputGroup, FormControl, Button, Alert, Badge } from "react-bootstrap";
 
@@ -10,6 +10,10 @@ function ElementoR({ elemento, onChange }) {
     elemento.toSimpleObj()
   );
   const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    setInternalElemento(elemento.toSimpleObj());
+  }, [elemento, elemento.attributi.length]);
 
   const handleChange = (attributoValore, valore) => {
     const attInternalElemento = { ...internalElemento };
