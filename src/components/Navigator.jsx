a34435370810194bac98561fc46e80658d062a90
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-12-09 23:39:06 +0100

    fix Collapse

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -4,18 +4,21 @@ import { NavLink } from "react-router-dom";
 
 function Navigator({ nomeDecisione }) {
   return (
-    <Navbar bg="light">
+    <Navbar bg="light" expand="lg">
       <Navbar.Brand as={NavLink} to="/">
         Be Prometeo
       </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link as={NavLink} to="/attuale">
-          {nomeDecisione}
-        </Nav.Link>
-        <Nav.Link as={NavLink} to="/lista-decisioni">
-          Decisioni
-        </Nav.Link>
-      </Nav>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/attuale">
+            {nomeDecisione}
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/lista-decisioni">
+            Tutte le decisioni
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
