88405216a68d50bcd27313a67f8a6eb38e1aae38
Riccardo Belingheri <riccardo.beli99@gmail.com>
2020-12-09 23:39:38 +0100

    use lista Decisioni

--- src/components/Routerig.jsx
+++ src/components/Routerig.jsx
@@ -1,26 +1,31 @@
 import PropTypes from "prop-types";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 import Decisione from "./Decisione";
+import ListaDecisioni from "./ListaDecisioni";
 
-function Routering({ actualDecisione }) {
+function Routering({ actualDecisione, nomeDecisione, ...rest }) {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/attuale" />
-      </Route>
-      <Route path="/attuale">
-        <Decisione decisione={actualDecisione} />
-      </Route>
-      <Route path="/lista-decisioni">
-        <h1>lavori in corso</h1>
-      </Route>
-    </Switch>
+    <Container>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/attuale" />
+        </Route>
+        <Route path="/attuale">
+          <Decisione decisione={actualDecisione} />
+        </Route>
+        <Route path="/lista-decisioni">
+          <ListaDecisioni nomeDecisione={nomeDecisione} {...rest} />
+        </Route>
+      </Switch>
+    </Container>
   );
 }
 
 Routering.propTypes = {
   actualDecisione: PropTypes.string.isRequired,
+  nomeDecisione: PropTypes.string.isRequired,
 };
 
 export default Routering;
