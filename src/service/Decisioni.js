e703e95c3118422955c2faaeae2ad95151b7f402
Belingheri <riccardo.beli99@gmail.com>
2021-01-03 14:37:51 +0100

    funzioni x aggiungere una nuova decisione

--- src/service/Decisioni.js
+++ src/service/Decisioni.js
@@ -1,3 +1,6 @@
+import Attributo from "../model/Attributo";
+import Elemento from "../model/Elemento";
+import AttributoValore from "../model/AttributoValore";
 const defaultDecisione =
   '{"attributi":[{"nome":"consumo","peso":7},{"nome":"potenza","peso":3}],"elementi":[{"descrizione":"panda","attributi":[{"nome":"consumo","peso":7,"valore":9},{"nome":"potenza","peso":3,"valore":1}]},{"descrizione":"ferrari","attributi":[{"nome":"consumo","peso":7,"valore":1},{"nome":"potenza","peso":3,"valore":10}]},{"descrizione":"fiesta","attributi":[{"nome":"consumo","peso":7,"valore":8},{"nome":"potenza","peso":3,"valore":6}]}]}';
 const defaultName = "DEFAULT";
@@ -57,4 +60,39 @@ function getAll() {
   );
 }
 
-export { get, getSelectedName, setSelectedName, save, getAll };
+/**
+ * @description Aggiunge una nuova decisione
+ * @param {string} nomeDecisione nome della nuova decisione
+ */
+function addDecisione(nomeDecisione) {
+  validateNomeDecisione(nomeDecisione);
+  // creo degli attributi fittizzi
+  const attributi = [new Attributo("default", Attributo.pesoMinimo)];
+  const attributiValore = [AttributoValore.creaAttributoValore(attributi[0])];
+  const elemento = new Elemento(nomeDecisione, attributiValore);
+  // fix
+  throw new Error("sistemami");
+  //save(elemento, attributi);
+}
+/**
+ * @desciprion controlla se il nome da assegnare è valido
+ * @param {string} nomeDecisione
+ * @throw errore per la quale non è valido
+ */
+function validateNomeDecisione(nomeDecisione) {
+  if (!nomeDecisione) throw new Error("Decisione deve essere valorizzato");
+  if (typeof nomeDecisione !== "string")
+    throw new Error("Decisione deve esere una stringa");
+  if (!nomeDecisione.match(/^\w+$/i))
+    throw new Error("Il nome può comprendere solo lettere, numeri e _");
+}
+
+export {
+  get,
+  getSelectedName,
+  setSelectedName,
+  save,
+  getAll,
+  addDecisione,
+  validateNomeDecisione,
+};
