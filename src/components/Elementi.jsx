9d2101e6451e2e3149465492fc5e806e2da84a64
Belingheri <riccardo.beli99@gmail.com>
2021-01-07 21:33:17 +0100

    Cancellazione di un elemento

--- src/components/Elementi.jsx
+++ src/components/Elementi.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Badge } from "react-bootstrap";
 import PropTypes from "prop-types";
 
+import ModalBox from "./common/ModalBox";
 import ElementoR from "./ElementoR";
 
 import Elemento from "../model/Elemento";
-import { Button } from "react-bootstrap";
 
-function Elementi({ elementi, onChange, onSaveDecisione }) {
+function Elementi({ elementi, onChange, onSaveDecisione, onDelete }) {
+  const [show, setShow] = useState(false);
+  const [elementoToDelete, setElementoToDelete] = useState("");
+  const bottoni = [
+    {
+      render: (
+        <Button variant="outline-danger">
+          <i className="far fa-trash-alt"></i>
+        </Button>
+      ),
+      id: "cancella",
+    },
+  ];
+  const handleCloseModal = (risposta) => {
+    setShow(false);
+    if (risposta === "cancella") onDelete(elementoToDelete);
+  };
+  const handleDeleteButton = (descrizioneElemento) => {
+    setElementoToDelete(descrizioneElemento);
+    setShow(true);
+  };
+  const modalBody = (
+    <p>
+      Eliminare definitivamente{" "}
+      <Badge variant="danger">{elementoToDelete}</Badge>
+    </p>
+  );
   const elementiOrdinati = elementi.sort((a, b) => b.valore - a.valore);
   return (
     <div>
@@ -22,9 +49,21 @@ function Elementi({ elementi, onChange, onSaveDecisione }) {
             key={elemento.descrizione}
             elemento={elemento}
             onChange={onChange}
+            onDelete={handleDeleteButton}
+            canDelete={elementiOrdinati.length > 1}
           />
         );
       })}
+
+      {elementiOrdinati.length > 1 && (
+        <ModalBox
+          show={show}
+          buttons={bottoni}
+          title="Attenzione"
+          body={modalBody}
+          onClose={handleCloseModal}
+        />
+      )}
     </div>
   );
 }
@@ -36,6 +75,7 @@ Elementi.propTypes = {
   onChange: PropTypes.func.isRequired,
   onFocusOut: PropTypes.func.isRequired,
   onSaveDecisione: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
   elementiErrors: PropTypes.object,
 };
 
